ac85b98d96e44dee479aa09b2b0fb308d14d296f
Yash02092003 <mg.anujtyagi1@gmail.com>
2024-05-19 15:37:18 +0530

    All

---
+++ app.js
@@ -0,0 +1,31 @@
+const exp = require('constants');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const { seedDoctor, seedPatient } = require('./seed');
+const app = express();
+const PORT = 8080;
+
+mongoose.connect('mongodb://localhost:27017/Hospital-Management')
+.then( () => {
+    console.log('Server Connected At PORT : 27017');
+})
+.catch( (e) => {
+    console.log('Some error Occurred' , e);
+})
+
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname , 'views'));
+app.use(express.static(path.join(__dirname , 'public')));
+
+
+// seedDoctor();
+// seedPatient();
+
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`Server Connected At PORT : ${PORT}`);
+})
---
+++ model/Doctor.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const {Schema , model} = mongoose;
+
+let doctorSchema = new Schema({
+    name : {
+        type : String ,
+        required : true ,
+        trim : true,
+    } ,
+    consultationFee : {
+        type : Number ,
+        required : true ,
+        trim : true,
+    } ,
+    comment : [
+        {
+            rating : {
+                type :  Number ,
+                min : 0 ,
+                max : 5
+            } ,
+            comment : {
+                type : String ,
+            }
+        }
+    ] ,
+    patientList : [{
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : 'Patient'
+    }] ,
+    appointmentList : [{
+        time : {
+            type : Date ,
+            required : true 
+        } ,
+        patient : {
+            type : mongoose.Schema.Types.ObjectId ,
+            ref : 'Patient'
+        }
+    }]
+})
+
+const Doctor = model('doctor' , doctorSchema);
+
+module.exports = Doctor;
