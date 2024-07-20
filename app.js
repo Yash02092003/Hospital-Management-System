8aec827573fcd3be45d07391ac3f5fceb0a5ec84
Yash02092003 <mg.anujtyagi1@gmail.com>
2024-07-21 00:18:06 +0530

    Save local changes before merging

--- app.js
+++ app.js
@@ -19,8 +19,13 @@ app.set('views' , path.join(__dirname , 'views'));
 app.use(express.static(path.join(__dirname , 'public')));
 
 
+
 // seedDoctor();
 // seedPatient();
+
+app.get('/' , (req , res) => {
+    res.render('doctor/index');
+})
 
 
 
